f243ab85d783f0194255821acc57e298d7b78730
velvermotosatlixco-ui <velvermotosatlixco@gmail.com>
2026-05-15 19:52:05 -0600

    Update catalogo.js

--- public/catalogo/catalogo.js
+++ public/catalogo/catalogo.js
@@ -1,1 +1,47 @@
+const motos = [
 
+  {
+    marca: "Ryder",
+    modelo: "RD-1",
+    precio: 20499,
+    categoria: "Trabajo",
+    cilindrada: 150,
+    imagen: "../ad1.5.webp",
+    descripcion: "Motocicleta confiable, económica y funcional para trabajo diario.",
+    credito: true
+  },
+
+  {
+    marca: "ZMoto",
+    modelo: "CRX 200",
+    precio: 35999,
+    categoria: "Adventure",
+    cilindrada: 200,
+    imagen: "../crx200.webp",
+    descripcion: "Diseño premium para aventura urbana y carretera.",
+    credito: true
+  },
+
+  {
+    marca: "ZMoto",
+    modelo: "300 Pro Sport",
+    precio: 52999,
+    categoria: "Deportiva",
+    cilindrada: 300,
+    imagen: "../crx300.png",
+    descripcion: "Potencia, presencia y estilo de alto impacto.",
+    credito: true
+  },
+
+  {
+    marca: "Ryder",
+    modelo: "Fighter 200",
+    precio: 38999,
+    categoria: "Sport",
+    cilindrada: 200,
+    imagen: "../fighter200.webp",
+    descripcion: "Estilo deportivo premium para clientes aspiracionales.",
+    credito: true
+  }
+
+];
