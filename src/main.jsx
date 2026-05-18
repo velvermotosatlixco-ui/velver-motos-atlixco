bdcd2cc085544e926ce2a84775e5f244926fc7a4
velvermotosatlixco-ui <velvermotosatlixco@gmail.com>
2026-05-18 16:04:57 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import './styles/globals.css';
+import './styles/tokens.css';
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
