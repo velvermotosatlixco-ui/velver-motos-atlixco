3bf7f92d07eb2356ec2bc81b300e5a8b65051ff3
velvermotosatlixco-ui <velvermotosatlixco@gmail.com>
2026-05-18 15:58:48 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
 
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
