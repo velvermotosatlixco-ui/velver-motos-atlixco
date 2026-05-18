e47ae3b47b5a65c4b040a9866cac1c2b2d39e568
velvermotosatlixco-ui <velvermotosatlixco@gmail.com>
2026-05-18 16:33:54 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h1>VELVER MOTOS</h1>
+      <div className="navbar-logo">
+        VELVER MOTOS™
+      </div>
+
+      <div className="navbar-links">
+        <a href="#">Inicio</a>
+        <a href="#">Catálogo</a>
+        <a href="#">Financiamiento</a>
+        <a href="#">Contacto</a>
+      </div>
     </nav>
   );
 }
